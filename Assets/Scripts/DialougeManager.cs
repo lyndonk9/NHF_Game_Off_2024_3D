cd0786ca05d036ebd3e4f0372e1a7311da9b3a85
ZeroskyNight <chris.ric.browne@gmail.com>
2024-11-21 00:46:43 -0500

    Got Dialouge plus animation to work
    Got Dialouge box working plus , opening and closing animation. Next is adding triggers

--- Assets/Scripts/DialougeManager.cs
+++ Assets/Scripts/DialougeManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro; // Add this at the top
 public class DialougeManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public TextMeshProUGUI dialougeText;
+    public Animator animator;
+
     private Queue<string> sentences;
 
     void Start()
@@ -15,6 +21,8 @@ public class DialougeManager : MonoBehaviour
     public void StartDialouge(Dialouge dialouge)
     {
         Debug.Log("Start Test" + dialouge.sentences);
+
+        animator.SetBool("IsOpen", true);
 
         sentences.Clear();
 
@@ -35,11 +43,36 @@ public class DialougeManager : MonoBehaviour
         }
 
         string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+        //dialougeText.text = sentence;
         Debug.Log(sentence);
+    }
+
+    //IEnumerator TypeSentence (string sentence)
+    //{
+    //    dialougeText.text = "";
+    //    foreach (char letter in sentence.ToCharArray()) 
+    //    {
+    //    dialougeText.text += letter;
+    //        yield return null;
+    //    }
+    //}
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialougeText.text = "";
+        float typingSpeed = 0.05f; // Adjust this value for the desired speed (e.g., 0.05 seconds per character).
+
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialougeText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
     }
 
     public void EndDialouge() 
     {
+        animator.SetBool("IsOpen", false);
         Debug.Log("End Text");
     }
 
